7f7088a2c237db77f5a354c2aebdb49f232d3b0f
Harshita Sharma <23053364@kiit.ac.in>
2025-07-19 20:30:57 +0530

    added analysis

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,12 +3,13 @@ import "../styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
 import Layout from "@/components/Layout"; // ✅ Make sure path is correct
-
+import { Analytics } from "@vercel/analytics/next"
 export default function App({ Component, pageProps }) {
   return (
     <ClerkProvider {...pageProps}>
       <Layout>
         <Component {...pageProps} />
+           <Analytics />
       </Layout>
       <Toaster position="top-right" />
     </ClerkProvider>
